72db39e47c5164ad86276facb1020b497dd7b4cd
Maryam Musa Isa <maryammusaisa021@gmail.com>
2022-07-02 10:30:56 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - prints all numbers in lowercase on a new line
+ *
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	int n;
+	char ch;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	for (ch = 'a'; ch <= 'f'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
